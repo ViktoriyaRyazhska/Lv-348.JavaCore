64a000b809e3618d91fdf950a2b3722914ce0aa4
Ruslan <ruslanzhezlo2@gmail.com>
2018-09-02 19:00:25 +0300

    Create Ex1

---
+++ HomeWork/Exercise1.java
@@ -0,0 +1,23 @@
+package Lesson1;
+
+
+import static java.lang.Math.*;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+	
+		int R = 3;
+		
+		double P = 2 * PI * R;
+		
+		double S = PI * (pow(R,2));
+		
+		
+		System.out.println("Perimeter bed is " + P);
+		
+		System.out.println("Area bed is " + S);
+
+	}
+
+}

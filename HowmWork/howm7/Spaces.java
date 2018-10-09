3c239339f4f4357ff4ead5fb3dd7bd1c47e1760b
Your Name <you@example.com>
2018-10-09 15:33:41 +0300

    add hw7

---
+++ HowmWork/howm7/Spaces.java
@@ -0,0 +1,26 @@
+package s³tging.howm;
+public class Spaces {
+
+	public static void main(String[] args) {
+		
+		String str = "I  am                learning          Java             Core";
+		System.out.println(str);
+		
+		while (str.contains("  ")) {
+			String s1 = str.replace("  ", " ");
+			str = s1;
+		}
+		System.out.println(str);
+		
+		while (str.contains("I am")) {
+			String s1 = str.replace("I am", "I'm");
+			str = s1;
+		}
+		System.out.println(str);
+		
+		String str1 = "My          name   is                   Nazar";
+		System.out.println(str1);
+		System.out.println(str1.replaceAll("[ ]+", " "));
+	}
+
+}
